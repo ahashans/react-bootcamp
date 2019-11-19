1f1e5f1154b39ffb648fc820755a66126677738f
Ahashan Alam Sojib <ahashan@linkstaff.co.jp>
2019-11-19 11:43:00 +0600

    Initial Commit with Pokedex

---
+++ pockedex/src/components/Pokedex.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Pokecard from "./Pokecard";
+
+class Pokedex extends Component {
+    render() {
+        return (
+            <div>
+                <Pokecard/>
+            </div>
+        );
+    }
+}
+
+export default Pokedex;
