c53f326c4f28540f206a1c168f39a71ba2b00906
Ahashan Alam Sojib <ahashans@gmail.com>
2019-11-27 22:59:09 +0600

    Added React Playground project

---
+++ state_playground/src/clicker.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+class Clicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            value:0
+        };
+        this.handleClick=this.handleClick.bind(this);
+    }
+    handleClick(e){
+        let rand = Math.floor(Math.random() * 10)+1;
+        this.setState({value:rand});
+    }
+    render() {
+        return (
+            <div>
+                <h1>Number is {this.state.value}</h1>
+                {this.state.value===7
+                    ?<h1>YOU WIN!</h1>
+                    : <button onClick={this.handleClick}>Random Number</button>
+                }
+            </div>
+        );
+    }
+}
+
+export default Clicker;
